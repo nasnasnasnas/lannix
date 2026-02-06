20929ea143911223ae259a24d26ba1137b14d09c
nea <git@nea.dev>
2026-02-05 22:05:50 -0500

    Make caddy secrets optional

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -3,7 +3,7 @@
     networks ? [],
     image ? "ghcr.io/caddybuilds/caddy-cloudflare:latest",
     caddyfilePath,
-    secretsEnvPath ? "/etc/caddy/secrets.env",
+    secretsEnvPath ? null,
     dataDir ? "/home/magicbox/data/caddy",
   }: {
     container_name = "caddy";
@@ -11,7 +11,8 @@
     restart = "always";
     domains = [];
     caddy_port = null;
-    command = ["caddy" "run" "--config" "/etc/caddy/Caddyfile" "--adapter" "caddyfile" "--envfile" "/etc/caddy/secrets.env"];
+    command = ["caddy" "run" "--config" "/etc/caddy/Caddyfile" "--adapter" "caddyfile"]
+      ++ (if secretsEnvPath == null then [] else ["--envfile" "/etc/caddy/secrets.env"]);
     inherit networks;
     ports = [
       "80:80"
@@ -20,8 +21,9 @@
     ];
     volumes = [
       "${caddyfilePath}:/etc/caddy/Caddyfile:ro"
-      "${secretsEnvPath}:/etc/caddy/secrets.env:ro"
       "${dataDir}:/data"
-    ];
+    ] ++ (if secretsEnvPath == null then [] else [
+      "${secretsEnvPath}:/etc/caddy/secrets.env:ro"
+    ]);
   };
 }
