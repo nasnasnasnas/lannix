ff5ba54b0e7875b0d42ce021b252c221d3943727
leah <catgirl@catgirlin.space>
2026-02-10 00:19:09 -0500

    fuc

--- modules/hosts/servers/magicplank/services.nix
+++ modules/hosts/servers/magicplank/services.nix
@@ -1,5 +1,5 @@
 {inputs, ...}: {
-  flake.modules.nixos.remotebox = inputs.self.lib.mkHostServices {
+  flake.modules.nixos.magicplank = inputs.self.lib.mkHostServices {
     publicIPs = ["107.219.61.126" "100.100.165.5"];
     services = with inputs.self.services; [
       (nextcloud {domains = ["https://next.szpunar.cloud"];})
