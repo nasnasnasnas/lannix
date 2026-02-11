115ed013d8f764d85eb4a73317f870cabcfaf47b
leah <catgirl@catgirlin.space>
2026-02-10 23:17:30 -0500

    hh

--- modules/apps/postgres-puppy.nix
+++ modules/apps/postgres-puppy.nix
@@ -2,15 +2,21 @@
   config.flake.lib.mkPostgresPuppy = {pkgs, databases}: let
     postgresPuppyScript = ./postgres-puppy/index.ts;
     databasesJson = builtins.toJSON (map (name: {inherit name;}) databases);
+    docker = "${pkgs.docker}/bin/docker";
   in
     pkgs.writeShellApplication {
       name = "postgres-puppy";
       runtimeInputs = [pkgs.bun];
       text = ''
+        until ${docker} exec postgres pg_isready -U postgres; do
+          echo "waiting for postgres..."
+          sleep 2
+        done
+
         # set the OP_SERVICE_ACCOUNT_TOKEN env to contents of /etc/op-token
         OP_SERVICE_ACCOUNT_TOKEN=$(cat /etc/op-token)
         export OP_SERVICE_ACCOUNT_TOKEN
         bun i @1password/sdk
-        bun run ${postgresPuppyScript} ${builtins.toJSON databasesJson} ${pkgs.docker}/bin/docker'';
+        bun run ${postgresPuppyScript} ${builtins.toJSON databasesJson} ${docker}'';
     };
 }
