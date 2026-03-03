ab8019624af801d3491226a7b07d1bae7fd03aca
leah <catgirl@catgirlin.space>
2026-03-03 10:11:59 -0500

    add dind

--- modules/hosts/servers/magicplank/forgejo-runner.nix
+++ modules/hosts/servers/magicplank/forgejo-runner.nix
@@ -28,6 +28,15 @@
       };
     };
 
+    virtualisation.arion.projects."forgejo-dind".settings.services."forgejo-dind".service = {
+      restart = "unless-stopped";
+      container_name = "forgejo-dind";
+      image = "docker:dind";
+      privileged = true;
+      command = ["dockerd" "-H" "tcp://0.0.0.0:2375" "--tls=false"];
+      ports = [ "127.0.0.1:2376:2375" ];
+    };
+
     services.gitea-actions-runner = {
       package = pkgs.forgejo-runner;
       instances.forgejo = {
@@ -41,8 +50,19 @@
           "bun-latest:docker://oven/bun:latest"
           "debian-latest:docker://debian:latest"
           "ubuntu-latest:docker://ubuntu:latest"
+          "rust-latest:docker://rust:latest"
         ];
-        # settings = { ... };
+        settings = {
+          runner = {
+            envs = {
+              DOCKER_HOST = "tcp://forgejo-dind.docker.internal:2375";
+            };
+          };
+          container = {
+            docker_host = "tcp://127.0.0.1:2376";
+            options = "--add-host=forgejo-dind.docker.internal:host-gateway";
+          };
+        };
       };
     };
   };
