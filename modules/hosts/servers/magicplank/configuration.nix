9abcad3d13e168e01ef361c3b1ac32aad4a3e055
leah <catgirl@catgirlin.space>
2026-06-23 02:54:45 -0400

    connect

--- modules/hosts/servers/magicplank/configuration.nix
+++ modules/hosts/servers/magicplank/configuration.nix
@@ -92,6 +92,7 @@
 
       displayManager.plasma-login-manager.enable = true;
     };
+    programs.kdeconnect.enable = true;
 
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
