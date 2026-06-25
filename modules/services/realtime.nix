79f86b97a5eaae84aab26ba9c19f31d045640c67
leah <catgirl@catgirlin.space>
2026-06-25 17:22:06 -0400

    update remotebox

---
+++ modules/services/realtime.nix
@@ -0,0 +1,24 @@
+{...}: {
+  flake.services.realtime-md = {
+    domains ? [],
+    networks ? [],
+    image ? "ghcr.io/nealol/realtime-server:latest",
+    volumes ? [],
+    dataDir ? "/home/magicbox/data/realtime",
+    envSecrets ? {},
+    environment ? {},
+    env_file ? [],
+  }: {
+    inherit domains;
+    inherit envSecrets;
+    container_name = "realtime";
+    environment = {
+      
+    } // environment;
+    inherit image;
+    restart = "unless-stopped";
+    inherit networks;
+    caddy_port = 8081;
+    volumes = volumes ++ [ "${dataDir}:/data" ];
+  } // (if env_file == [] then {} else {inherit env_file;});
+}
