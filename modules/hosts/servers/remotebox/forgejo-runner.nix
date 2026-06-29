78aeba02c3ef40a76d1dc4e3818a5470beec48f3
leah <catgirl@catgirlin.space>
2026-06-29 01:56:03 -0400

    deploys!!

---
+++ modules/hosts/servers/remotebox/forgejo-runner.nix
@@ -0,0 +1,50 @@
+{inputs, ...}: {
+  flake.modules.nixos.remotebox = {pkgs, ...}: {
+    services.onepassword-secrets = {
+      enable = true;
+      secrets = {
+        forgejoRunnerToken = {
+          path = "/var/lib/opnix/secrets/forgejo-runner/token";
+          reference = "op://Secrets/Forgejo Runner Registration Token/password";
+          mode = "0640";
+        };
+      };
+    };
+
+    systemd.services.forgejo-runner-token-env = {
+      description = "Prepare forgejo runner token env file";
+      after = ["opnix-secrets.service"];
+      requires = ["opnix-secrets.service"];
+      before = ["gitea-runner-forgejo.service"];
+      wantedBy = ["gitea-runner-forgejo.service"];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = pkgs.writeShellScript "forgejo-runner-token-env" ''
+          install -Dm 0640 /dev/null /var/lib/opnix/secrets/forgejo-runner/token-env
+          printf 'TOKEN=%s' "$(cat /var/lib/opnix/secrets/forgejo-runner/token)" \
+            > /var/lib/opnix/secrets/forgejo-runner/token-env
+        '';
+      };
+    };
+
+    services.gitea-actions-runner = {
+      package = pkgs.forgejo-runner;
+      instances.forgejo = {
+        enable = true;
+        name = "forgejo-runner-remotebox";
+        tokenFile = "/var/lib/opnix/secrets/forgejo-runner/token-env";
+        url = "https://git.szpunar.cloud/";
+        labels = [
+          "remotebox-native:host"
+        ];
+        hostPackages = with pkgs; [
+          bash coreutils curl gawk git gnused nodejs wget nix attic-client nix-fast-build jq
+        ];
+        settings = {
+          runner.capacity = 2;
+        };
+      };
+    };
+  };
+}
