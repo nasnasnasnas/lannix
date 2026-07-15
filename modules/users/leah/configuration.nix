9d866b42fa9ac2e38e8afe6251bec118ef773cdd
leah <catgirl@catgirlin.space>
2026-07-15 12:48:17 -0400

    add some other little things

--- modules/users/leah/configuration.nix
+++ modules/users/leah/configuration.nix
@@ -30,6 +30,12 @@ in {
     };
     programs.fish.enable = true;
 
+    environment.systemPackages = with pkgs; [
+      _1password-gui
+      _1password-cli
+      unstable.bun
+    ];
+
     fonts.enableDefaultPackages = true;
     fonts.packages = with pkgs; [
       noto-fonts
